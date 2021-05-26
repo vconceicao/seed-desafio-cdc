8b1308ba72f9d67b00c858904ccac9f9f54d686a
Vinicius <vinc-analist@hotmail.com>
2021-05-25 21:53:47 -0300

    Initial commit

---
+++ src/test/java/com/desafio/cdc/CdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.cdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
